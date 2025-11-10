007cb88ed4e6c5d329292ec80ff5d5ebe2d09b2a
e <saurabhmandhalkar03@gmail.com>
2025-11-10 18:05:42 +0530

    Day 11 - Added JS in Project 1 and 2

--- Project-1-Snake-water-gun-game/app/func/app.js
+++ Project-1-Snake-water-gun-game/app/func/app.js
@@ -1,7 +1,49 @@
+// Selecting DOM elements
 const select = document.querySelector("select")
-const option = document.querySelector("option")
-const btn = document.querySelector("button")
+const resultBox = document.getElementById("result-box");
+const form = document.querySelector("form");
+const list = document.querySelector("ul")
 
-option.addEventListener("click", function () {
-    console.log("Detected!!");
+
+// Game options
+const choices = ["snake", "water", "gun"]
+
+
+// Function to get computer choice`
+function getComputerChoice(){
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+} 
+
+
+// Function to decide winner
+function getWinner(user, computer){
+    if (user == computer) return "It's a tie.";
+
+    if ((user === "snake" && computer === "water") ||
+        (user === "water" && computer === "gun") ||
+        (user === "gun" && computer === "snake")){
+            return "You win!!";
+    }
+    else{
+        return "Computer Wins!!";
+    }
+}
+
+
+// Event listener for form submission
+form.addEventListener("submit", function (e) {
+    e.preventDefault();  // Stops form from reloading
+
+    const userChoice = select.value;
+    const computerCHoice = getComputerChoice();
+    const result = getWinner(userChoice, computerCHoice);
+
+    // Display result in result box;
+    resultBox.textContent = `You chose ${userChoice} | Computer chose ${computerCHoice} -> ${result}`;
+
+    // Adding result to list
+    const listItem = document.createElement("li");
+    listItem.textContent = `User ${userChoice} VS Computer ${computerCHoice} -> ${result}`;
+    list.appendChild(listItem);
 })
