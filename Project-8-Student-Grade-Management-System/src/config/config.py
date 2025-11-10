9e8d8a80f649ae5e6beb9ee3407c707ccde82931
e <saurabhmandhalkar03@gmail.com>
2025-11-11 02:36:17 +0530

    Day 12 - Continued with DB in Project 8

---
+++ Project-8-Student-Grade-Management-System/src/config/config.py
@@ -0,0 +1,15 @@
+'''
+    This file reads the environment variable
+'''
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Setting(BaseSettings):
+    DATABASE_URL : str 
+    
+    model_config = SettingsConfigDict(
+        env_file="src/.env",
+        extra="ignore"
+    )
+    
+config = Setting()
---
+++ Project-8-Student-Grade-Management-System/src/db/db.py
@@ -0,0 +1,28 @@
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from config.config import config
+
+# creating async database engine using asyncpg
+async_engine = create_async_engine(
+    config.DATABASE_URL,    # Reading DB url from .env
+    echo=True       # displaying SQL qureis
+)
+
+# Function to initialize db and create table
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        # Automatically creates all the tables in models
+        await conn.run_sync(SQLModel.metadata.create_all)
+        
+        
+# # Dependancy: returns an async session (used by FASTAPI routes)
+async def get_session() -> AsyncSession:
+    sessionLocal = sessionmaker(
+        async_engine,
+        calss_=AsyncSession,
+        expire_on_commit=False
+    )
+    
+    async with sessionLocal() as session:
+        yield session
